2d033705c9c333e1c7e0a8aa5c0a9841781db4c7
Ramón Hernández García <rmnhg16@gmail.com>
2017-08-12 15:05:41 +0200

    LineageOS for tianchi
    Vendor setup updated

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2013 The Android Open Source Project
+# Copyright 2017 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,5 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_d5303-userdebug
-
+add_lunch_combo lineage_tianchi-userdebug
